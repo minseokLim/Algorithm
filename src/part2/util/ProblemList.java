87757440c5378d98a20172672384b474842790b6
mslim8803 <mslim8803@kaoni.com>
2019-11-13 12:43:57 +0900

    문제 리스트 및 풀이현황 유틸 추가

---
+++ src/part2/util/ProblemList.java
@@ -0,0 +1,23 @@
+package part2.util;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ProblemList {
+	// DP
+	final static List<Integer> dpList = Arrays.asList(1463, 11726, 11727, 9095, 10844, 11057, 2193, 9465, 2156, 11053, 11055, 11722, 11054, 1912, 2579, 
+														1699, 2133, 9461, 2225, 2011, 11052);
+	// 수리
+	final static List<Integer> mathList = Arrays.asList(2751, 11650, 11651, 10814, 10825, 10989, 11652, 11004, 10828, 9012, 10799, 10845, 10866, 10808, 
+														  10809, 10820, 2743, 11655, 10824, 11656, 1406, 1158, 1168, 10430, 2609, 1934, 1850, 9613, 11005, 
+														  2745, 1373, 1212, 2089, 11576, 1978, 1929, 6588, 11653, 10872, 1676, 2004);
+	// 그래프
+	final static List<Integer> graphList = Arrays.asList(1260, 11724, 1707, 10451, 2331, 9466, 2667, 4963, 7576, 2178, 2146, 1991, 11725, 1167, 1967);
+	// 이분탐색, 삼분탐색
+	final static List<Integer> pSearchList = Arrays.asList(1654, 2805, 2110, 10815, 10816, 11662);
+	// 그리디
+	final static List<Integer> greedyList = Arrays.asList(11047, 2875, 10610, 1783, 1931, 11399, 2873, 1744);
+	// 완전탐색
+	final static List<Integer> fSearchList = Arrays.asList(1476, 1107, 1451, 10819, 10971, 1697, 1963, 9019, 1525, 2251, 2186, 3108, 5014, 1759, 2580, 1987, 
+													 6603, 1182, 2003, 1806, 1644, 1261, 1208, 7453, 2632, 2143);
+}

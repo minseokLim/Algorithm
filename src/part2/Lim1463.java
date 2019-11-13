0b20f0c455b2fefb793a0c0448e0f9b6b2e38014
minseoklim <ddalgu2@gmail.com>
2019-11-13 21:04:30 +0900

    Lim

---
+++ src/part2/Lim1463.java
@@ -0,0 +1,34 @@
+package part2;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Lim1463 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int[] ans = new int[n + 1];
+		
+		ans[1] = 0;
+		
+		for(int i = 2; i <= n; i++) {
+			int min = Integer.MAX_VALUE;
+			
+			if(i % 3 == 0) {
+				min = ans[i / 3]; 
+			}
+			
+			if(i % 2 == 0 && ans[i / 2] < min) {
+				min = ans[i / 2];
+			}
+			
+			if(ans[i - 1] < min) {
+				min = ans[i - 1];
+			}
+			
+			ans[i] = min + 1;
+		}
+		
+		System.out.println(ans[n]);
+	}
+}

5dee2f0dc5152aec324a89ff9b57e889c49b0719
mslim <mslim8803@naver.com>
2019-11-15 09:07:09 +0900

    Lim

--- src/part2/needToReview/Lim2156.java
+++ src/part2/needToReview/Lim2156.java
@@ -1,9 +1,9 @@
-package part2;
+package part2.needToReview;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-public class _Lim2156 {
+public class Lim2156 {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
@@ -22,12 +22,12 @@ public class _Lim2156 {
 		}
 		
 		int[][] ans = new int[3][n];
-		ans[0][0] = 0;
-		ans[1][0] = liquor[0] == 0 ? -1 : liquor[0];
-		ans[2][0] = -1;
+		ans[0][1] = liquor[0];
+		ans[1][1] = liquor[1];
+		ans[2][1] = liquor[0] + liquor[1];
 		
 		for(int i = 2; i < n; i++) {
-			ans[0][i] = ans[2][i - 1];
+			ans[0][i] = Math.max(Math.max(ans[2][i - 1], ans[1][i - 1]), ans[0][i - 1]);
 			ans[1][i] = ans[0][i - 1] + liquor[i];
 			ans[2][i] = ans[1][i - 1] + liquor[i];
 		}

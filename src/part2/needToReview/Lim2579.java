037579107f8c6bf6078be4124fb5c0e8a2873590
mslim <mslim8803@naver.com>
2019-11-24 00:16:27 +0900

    Lim

--- src/part2/needToReview/Lim2579.java
+++ src/part2/needToReview/Lim2579.java
@@ -2,14 +2,26 @@ package part2.needToReview;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-public class _Lim2579 {	
+public class Lim2579 {	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-		int[] scores = new int[n];
+		int[] scores = new int[300];
 		
 		for(int i = 0; i < n; i++) {
 			scores[i] = Integer.parseInt(br.readLine());
 		}
+		
+		int[] max = new int[300];
+		
+		max[0] = scores[0];
+		max[1] = scores[0] + scores[1];
+		max[2] = Math.max(scores[0], scores[1]) + scores[2];
+		
+		for(int i = 3; i < n; i++) {
+			max[i] = Math.max(scores[i - 1] + max[i - 3], max[i - 2]) + scores[i];
+		}
+		
+		System.out.println(max[n - 1]);
 	}
 }

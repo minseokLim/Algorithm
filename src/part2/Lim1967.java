44e73ed3e6b9917e2013a14bd4c8ff025cb0d43d
mslim <mslim8803@naver.com>
2019-12-14 15:08:37 +0900

    submit 1967

--- src/part2/Lim1967.java
+++ src/part2/Lim1967.java
@@ -1,4 +1,4 @@
-package part2.needToReview;
+package part2;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -6,39 +6,34 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
 
-public class Lim1167 {
+public class Lim1967 {
 	private static int diameter = 0;
 	private static int lineEnd = 1;
 	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int v = Integer.parseInt(br.readLine());
-		
+		int n = Integer.parseInt(br.readLine());
 		@SuppressWarnings("unchecked")
-		List<Line>[] lists = new ArrayList[v + 1];
+		List<Line>[] lists = new ArrayList[n + 1];
 		
-		for(int i = 1; i <= v; i++) {
+		for(int i = 1; i <= n; i++) {
 			lists[i] = new ArrayList<Line>();
 		}
 		
-		for(int i = 0; i < v; i++) {
+		for(int i = 0; i < n - 1; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int len = Integer.parseInt(st.nextToken());
 			
-			while(st.hasMoreTokens()) {
-				int b = Integer.parseInt(st.nextToken());
-				
-				if(b == -1) break;
-				
-				int len = Integer.parseInt(st.nextToken());
-				lists[a].add(new Line(b, len));
-			}
+			lists[a].add(new Line(b, len));
+			lists[b].add(new Line(a, len));
 		}
 		
-		boolean[] visited = new boolean[v + 1];
+		boolean[] visited = new boolean[n + 1];
 		dfs(lists, visited, lineEnd, 0);
 		
-		visited = new boolean[v + 1];
+		visited = new boolean[n + 1];
 		dfs(lists, visited, lineEnd, 0);
 		
 		System.out.println(diameter);
@@ -58,9 +53,6 @@ public class Lim1167 {
 			
 			if(!visited[dest]) {
 				dfs(lists, visited, dest, length + len);
-				// 트리는 사이클이 없는 무방향 그래프
-				// 트리에서는 어떤 두 노드를 선택해도 둘 사이에 경로가 항상 하나만 존재하게 된다라고 한다 -> visited를 굳이 false로 원복시킬 이유가 없음. 
-				// visited[dest] = false; 
 			}
 		}
 	}

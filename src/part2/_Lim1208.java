41380e1694b8aa48db311dff34e971ccd0526287
mslim <mslim8803@naver.com>
2020-01-22 08:51:51 +0900

    Lim

--- src/part2/_Lim1208.java
+++ src/part2/_Lim1208.java
@@ -2,10 +2,12 @@ package part2;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class _Lim1208 {
 	private static int cnt = 0;
+	private static int index = 0;
 	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -21,20 +23,49 @@ public class _Lim1208 {
 			seq[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		for(int i = 0; i < n; i++) {
-			dfs(seq, seq[i], i, n, s);
+		if(n == 1) {
+			if(seq[0] == s) {
+				System.out.println(1);
+			} else {
+				System.out.println(0);
+			}
 		}
+		
+		int[] left = new int[n / 2];
+		System.arraycopy(seq, 0, left, 0, left.length);
+		int[] right = new int[n - n / 2];
+		System.arraycopy(seq, left.length, right, 0, right.length);
+		
+		int[] leftSumSet = new int[1 << left.length];
+		int[] rightSumSet = new int[1 << right.length];
+		leftSumSet[0] = 0;
+		rightSumSet[0] = 0;
+		
+		index = 1;
+		
+		for(int i = 0; i < left.length; i++) {
+			dfs(left, left[i], i, leftSumSet, s);
+		}
+		
+		index = 1;
+		
+		for(int i = 0; i < right.length; i++) {
+			dfs(right, right[i], i, rightSumSet, s);
+		}
+		
+		Arrays.sort(leftSumSet);
+		Arrays.sort(rightSumSet);
+		
+		int leftIdx
 		
 		System.out.println(cnt);
 	}
 
-	private static void dfs(int[] seq, int sum, int i, int n, int s) {
-		if(sum == s) {
-			cnt++;
-		}
+	private static void dfs(int[] seq, int sum, int i, int[] sumSet, int s) {
+		sumSet[index++] = sum;
 		
-		for(int j = i + 1; j < n; j++) {
-			dfs(seq, sum + seq[j], j, n, s);
+		for(int j = i + 1; j < seq.length; j++) {
+			dfs(seq, sum + seq[j], j, sumSet, s);
 		}
 	}
 }

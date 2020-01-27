acc5a94a684c30f65f80da1b5cbd5a129c7cd488
mslim <mslim8803@naver.com>
2020-01-27 19:43:36 +0900

    3번째 알고리즘 리뷰 준비

---
+++ src/part3/questionList/Lim11662.java
@@ -0,0 +1,62 @@
+package part3.questionList;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Lim11662 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		double aX = Double.parseDouble(st.nextToken());
+		double aY = Double.parseDouble(st.nextToken());
+		double bX = Double.parseDouble(st.nextToken());
+		double bY = Double.parseDouble(st.nextToken());
+		double cX = Double.parseDouble(st.nextToken());
+		double cY = Double.parseDouble(st.nextToken());
+		double dX = Double.parseDouble(st.nextToken());
+		double dY = Double.parseDouble(st.nextToken());
+		
+		int t = 1000_000;
+		
+		double dx1 = (bX - aX) / t;
+		double dy1 = (bY - aY) / t;
+		double dx2 = (dX - cX) / t;
+		double dy2 = (dY - cY) / t;
+		
+		double min = Double.MAX_VALUE;
+		int lo = 0;
+		int hi = t;
+		
+		while(hi - lo >= 3) {
+			int p = (2 * lo + hi) / 3;
+			int q = (lo + 2 * hi) / 3;
+			
+			double distP = getDistance(aX + dx1 * p, aY + dy1 * p, cX + dx2 * p, cY + dy2 * p);
+			double distQ = getDistance(aX + dx1 * q, aY + dy1 * q, cX + dx2 * q, cY + dy2 * q);
+			
+			if(distP < distQ) {
+				hi = q;
+			} else {
+				lo = p;
+			}
+		}
+		
+		while(lo <= hi) {
+			double distance = getDistance(aX + dx1 * lo, aY + dy1 * lo, cX + dx2 * lo, cY + dy2 * lo);
+			
+			if(distance < min) {
+				min = distance;
+			}
+			
+			lo++;
+		}
+				
+		System.out.println(min);
+	}
+	
+	private static double getDistance(double x1, double y1, double x2, double y2) {
+		double distance = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+		return distance;
+	}
+}
---
+++ src/part3/questionList/Lim1931.java
@@ -0,0 +1,79 @@
+package part3.questionList;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Lim1931 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		List<Meet> list = new ArrayList<Meet>();
+		
+		for(int i = 0; i < n; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int start = Integer.parseInt(st.nextToken());
+			int end = Integer.parseInt(st.nextToken());
+			
+			list.add(new Meet(start, end));
+		}
+		
+		Collections.sort(list);
+		
+		int cursor = 0;
+		int cnt = 0;
+		
+		for(int i = 0; i < n; i++) {
+			Meet meet = list.get(i);
+			int start = meet.getStart();
+			int end = meet.getEnd();
+			
+			if(start >= cursor) {
+				cnt++;
+				cursor = end;
+			}
+		}
+		
+		System.out.println(cnt);
+	}
+	
+	private static class Meet implements Comparable<Meet> {
+		private int start;
+		private int end;
+
+		public Meet(int start, int end) {
+			this.start = start;
+			this.end = end;
+		}
+
+		@Override
+		public int compareTo(Meet meet) {
+			int ret = end - meet.getEnd();
+			
+			if(ret == 0) {
+				ret = start - meet.getStart();
+//				ret = meet.getStart() - start;
+//				처음에 이와 같이 생각하기 쉬우나 이 경우 회의 시간이 0인, 시작 시간과 종료 시간이 같은 회의에 대한 대응이 이루어질 수 없다.
+//				[12, 14], [14, 14] 중에 [14, 14]를 앞에 정렬하게 되면 cursor가 14로 옮겨지는데, 그렇게 되면 [12, 14] 회의도 배정할 수 있음에도 불구하고 배정할 수 없다는 결과값이 도출된다.
+			}
+			
+			return ret;
+		}
+
+		public int getStart() {
+			return start;
+		}
+
+		public int getEnd() {
+			return end;
+		}
+
+//		@Override
+//		public String toString() {
+//			return "[start=" + start + ", end=" + end + "]";
+//		}
+	}
+}

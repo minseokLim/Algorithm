a72f9edb6f61684273d38a1cea5d88516d3aae42
mslim <mslim8803@naver.com>
2020-02-18 08:36:30 +0900

    Lim

---
+++ src/part3/_Lim2579.java
@@ -0,0 +1,16 @@
+package part3;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class _Lim2579 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int[] scores = new int[n];
+		
+		for(int i = 0; i < n; i++) {
+			scores[i] = Integer.parseInt(br.readLine());
+		}
+	}
+}

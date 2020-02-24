6236ab911396d73d75dccbe064d78d96a8079926
minseoklim <ddalgu2@gmail.com>
2020-02-24 17:52:47 +0900

    submit 1167

--- src/part3/needToReview/Lim11053_2.java
+++ src/part3/needToReview/Lim11053_2.java
@@ -1,38 +1,36 @@
-package part3;
+package part3.needToReview;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Lim11053 {
+public class Lim11053_2 {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
 		int[] seq = new int[n];
+		int[] partSeq = new int[n];
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
 		for(int i = 0; i < n; i++) {
 			seq[i] = Integer.parseInt(st.nextToken());
+			partSeq[i] = -1;
 		}
 		
-		int[] maxes = new int[n];
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < n; j++) {
+				if(partSeq[j] == -1 || partSeq[j] >= seq[i]) {
+					partSeq[j] = seq[i];
+					break;
+				}
+			}
+		}
+		
 		int answer = 0;
 		
 		for(int i = 0; i < n; i++) {
-			maxes[i] = 1;
-			
-			for(int j = 0; j < i; j++) {
-				if(seq[i] > seq[j]) {
-					int tmp = maxes[j] + 1;
-					
-					if(tmp > maxes[i]) {
-						maxes[i] = tmp;
-					}
-				}
-			}
-			
-			if(maxes[i] > answer) {
-				answer = maxes[i];
+			if(partSeq[i] != -1) {
+				answer++;
 			}
 		}
 		

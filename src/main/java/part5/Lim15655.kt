08a48631ac89ede4dbd819940db06612e6d1ea12
minseoklim <ddalgu2@gmail.com>
2022-02-02 10:34:40 +0900

    submit 15654, 15655

---
+++ src/main/java/part5/Lim15655.kt
@@ -0,0 +1,42 @@
+package part5
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.StringJoiner
+
+private val answers = StringJoiner(System.lineSeparator())
+private var n = 0
+private var m = 0
+private lateinit var numbers: List<Int>
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val inputs = br.readLine().mapToIntList()
+    n = inputs[0]
+    m = inputs[1]
+
+    numbers = br.readLine().mapToIntList().sorted()
+
+    for (i in 0 until n) {
+        solve(i, 1, StringBuilder())
+    }
+
+    println(answers)
+}
+
+private fun solve(srcIdx: Int, depth: Int, result: StringBuilder) {
+    result.append("${numbers[srcIdx]} ")
+
+    if (depth == m) {
+        answers.add(result.trim().toString())
+        return
+    }
+
+    for (i in srcIdx + 1 until n) {
+        solve(i, depth + 1, StringBuilder(result))
+    }
+}
+
+private fun String.mapToIntList(): List<Int> {
+    return this.split(" ").map { it.toInt() }
+}

620b488689ea0c31b2cc6eab4d75ce7967432e0e
minseoklim <ddalgu2@gmail.com>
2022-04-10 19:17:55 +0900

    (boj) : submit 14888

---
+++ src/main/java/boj/part5/Lim14888.py
@@ -0,0 +1,51 @@
+def dfs(operator_idx, seq_idx, value):
+    global min_value, max_value, seq, operator_count
+
+    operator_count[operator_idx] -= 1
+
+    if operator_idx == 0:
+        value = value + seq[seq_idx]
+    elif operator_idx == 1:
+        value = value - seq[seq_idx]
+    elif operator_idx == 2:
+        value = value * seq[seq_idx]
+    else:
+        value = int(value / seq[seq_idx])
+
+    if seq_idx == len(seq) - 1:
+        if min_value > value:
+            min_value = value
+        if max_value < value:
+            max_value = value
+
+        return
+
+    j = 0
+    while j < 4:
+        counts = 0
+        while operator_count[j] > 0:
+            dfs(j, seq_idx + 1, value)
+            counts += 1
+
+        operator_count[j] += counts
+        j += 1
+
+
+n = int(input())
+seq = list(map(int, input().split()))
+operator_count = list(map(int, input().split()))
+min_value = 10_0000_0000
+max_value = -10_0000_0000
+
+i = 0
+while i < 4:
+    count = 0
+    while operator_count[i] > 0:
+        dfs(i, 1, seq[0])
+        count += 1
+
+    operator_count[i] += count
+    i += 1
+
+print(max_value)
+print(min_value)

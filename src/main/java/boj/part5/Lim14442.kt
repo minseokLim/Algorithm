b4ec8dd0f1e05f0a20b3fd0997700e5b80138ea5
minseoklim <ddalgu2@gmail.com>
2022-05-08 11:46:44 +0900

    (boj) : submit 14442

---
+++ src/main/java/boj/part5/Lim14442.kt
@@ -0,0 +1,56 @@
+package boj.part5
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.LinkedList
+import java.util.Queue
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val (n, m, k) = br.readLine().split(" ").map { it.toInt() }
+    val map = Array(n) { BooleanArray(m) }
+
+    for (i in 0 until n) {
+        map[i] = br.readLine().map { it == '1' }.toBooleanArray()
+    }
+
+    val visited = Array(n) { Array(m) { BooleanArray(k + 1) } }
+
+    class Mover(
+        val i: Int,
+        val j: Int,
+        val breakableCount: Int,
+        val times: Int = 1
+    ) {
+        fun getNext(): List<Mover> {
+            return listOf(i - 1 to j, i + 1 to j, i to j - 1, i to j + 1)
+                .filter { it.first in 0 until n && it.second in 0 until m && !visited[it.first][it.second][breakableCount] && (breakableCount > 0 || !map[it.first][it.second]) }
+                .map {
+                    visited[it.first][it.second][breakableCount] = true
+
+                    if (breakableCount > 0 && map[it.first][it.second]) {
+                        Mover(it.first, it.second, breakableCount - 1, times + 1)
+                    } else {
+                        Mover(it.first, it.second, breakableCount, times + 1)
+                    }
+                }
+        }
+    }
+
+    val queue: Queue<Mover> = LinkedList()
+    queue.offer(Mover(0, 0, k))
+    visited[0][0][k] = true
+
+    while (queue.isNotEmpty()) {
+        val polled = queue.poll()
+
+        if (polled.i == n - 1 && polled.j == m - 1) {
+            println(polled.times)
+            return
+        }
+
+        queue.addAll(polled.getNext())
+    }
+
+    println(-1)
+}
